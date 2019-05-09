13ebfbd0ce98923081688d96f1ee9007c855423a
Yura <yurakatola@gmail.com>
2019-05-09 16:33:51 +0300

    Bonus task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
+    private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Please, enter command with arguments: ");
+        String input = sc.nextLine();
+        String[] inputArray = input.split(" ");
+
+        if (inputArray.length < 1) {
+            throw new IllegalArgumentException("Input should contain at least one word!");
+        }
+
+        String command = inputArray[0];
+        String[] parameters = new String[inputArray.length - 1];
+        int count = 1;
+        while (count < inputArray.length) {
+            parameters[count - 1] = inputArray[count];
+            count++;
+        }
+        System.out.println(String.format("Entered command = <%s>, parameters = %s", command, Arrays.toString(parameters)));
     }
 }
